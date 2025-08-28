2ee933da39631c5e438fca68e5a0f125e1127862
Virendra Kumar Dwivedi <dwivedivirendrakumar23@gmail.com>
2025-08-28 17:15:40 +0530

    class9 in js

---
+++ class9/script.js
@@ -0,0 +1,170 @@
+// for(let i=0;i<=2;i++)
+// {
+//     for(let j=0;j<=1;j++)
+//     {
+//         console.log("Jai Shree Ram");
+//     }
+// }
+
+// for(let v=0;v<=5;v++)
+// {
+//     for(let k=0;k<=5;k++)
+//     {
+//         console.log("Radhe Radhe");
+//     }
+// }
+
+// for(let v=0;v<=6;v++)
+// {
+//     for(j=0;j<=6;j++)
+//     {
+//         console.log("Har Har Mahadev");
+//     }
+//     console.log("\n");
+// }
+
+
+
+// for(let i=0;i<=4;i++)
+// {
+//     for(j=0;j<=4;j++)
+//     {
+//         console.log("4")
+//     }
+// }
+
+
+
+// //program  //
+
+// for(let i=0;i<3;i++)
+// {
+//     let veer="";
+//     for(let j=0;j<3;j++)
+//     {
+//         veer+="*";
+//     }
+//     console.log(veer);
+// }
+
+
+// for(let i=1;i<=5;i++)
+// {
+//     let ans="";
+//     for(let j=1;j<=i;j++)
+//     {
+//         ans=ans+="*";
+//     }
+//     console.log(ans)
+// }
+
+
+// for(let i=0;i<=5;i++)
+// {
+//     let veer="";
+//     for(let j=1;j<5-i;j++)
+//     {
+//         veer=veer+"*";
+//     }
+//     console.log(veer)
+// }
+
+
+// for(let i=1;i<=5;i++)
+// {
+//     let veer="";
+//     for(let j=1;j<5-i+1;j++)
+//     {
+//         veer=veer+j;
+//     }
+//     console.log(veer)
+// }
+
+// for(let i=1;i<=5;i++)
+// {
+//     let veer="";
+//     for(let j=5;j>=i;j--)
+//     {
+//         veer=veer+j;
+//     }
+//     console.log(veer)
+// }
+
+// for(let i=1;i<=5;i++)
+// {
+//     let veer=" ";
+//     for(let j=1;j<5-i+1;j++)
+//     {
+//         veer=veer+i;
+//     }
+//     console.log(veer)
+// }
+
+// for(let i=0;i<5;i++)
+// {
+//     let veer="";
+//     for(let space=0;space<i;space++)
+//     {
+//         veer+=" ";
+//     }
+//     let ans=" "
+//     for(let j=0;j<5-i;j++)
+//     {
+//         veer+=5-i-j;
+//     }
+//     console.log(veer)
+// }
+
+
+// for(let i=1;i<=4;i++)
+// {
+//     let ans="";
+//     let sp="";
+//     for(let space=1;space<5-i;space++)
+//     {
+//         sp+=" ";
+//     }
+//     for(let j=1;j<=2*i-1;j++)
+//     {
+//         ans+="*";
+//     }
+//     console.log(sp+ans);
+// }
+
+// for(let i=1;i<=4;i++)
+// {
+//     let ans="";
+//     let sp="";
+//     for(let space=1;space<5-i;space++)
+//     {
+//         sp+=" ";
+//     }
+//     for(let j=1;j<=2*i-1;j++)
+//     {
+//         ans+="*";
+//     }
+//     console.log(sp+ans);
+// }
+
+
+for(let i=1;i<=4;i++)
+{
+    let ans="";
+    let sp=""
+    for(let space=1;space<=4;space++)
+    {
+        sp+="";
+    }
+    for(let j=1;j<=2*i-;j++)
+    {
+        if(i==1||i==4||j==1||j==4)
+        {
+            ans+="*";
+        }
+        console.log(sp+ans)
+    }
+}
+
+
+
+
