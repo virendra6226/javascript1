33b4fb8499583a572cce5325e615a94ffb43c353
Virendra Kumar Dwivedi <dwivedivirendrakumar23@gmail.com>
2025-09-25 21:29:29 +0530

    javascript class22

---
+++ class22/script.js
@@ -0,0 +1,71 @@
+let arr=[23,45,56,76,23];
+console.log(arr);
+console.log(arr.length);
+console.log(arr[2]);
+
+console.log()
+
+// push method end element add
+
+let market=["virendra","kumar","Dwivedi"];
+market.push("veeru","veer","jitendra")
+console.log(market);
+
+
+let arr3=["virendra is back","veeru bhai", "virendra is not return"];
+arr.push("ram","krishna","vishnu","mahadev");
+console.log(arr3);
+
+// pop method delete last element
+
+let arr2=["virendra is back","veeru bhai", "virendra is not return"];
+arr.pop();
+console.log(arr2);
+
+
+let mark1=[2,3,4,5,5];
+mark.pop();
+console.log(mark1);
+
+let num=[34,32,45,44,34,54];
+console.log(num.slice(2,4));
+
+
+
+let veer = ["microsoft", "google", "oracle", "amazon"];
+veer.splice(2,2,"wipro","cognizant");
+console.log(veer);
+
+
+
+
+let str = "virendra kumar dwivedi";
+let reversed = str.split("").reverse().join("")
+console.log(reversed)
+
+
+let arr1=["hello","virendra","dwivedi"];
+let arr4=["veer","dwivedi","veeru", "dwivedi"];
+// let mergeArray=[...arr1, ...arr4];
+// console.log(mergeArray);
+
+let mark2=arr1.concat(arr2);
+console.log(mark);
+
+let mark=[23,34,56,67];
+let ans=mark.slice(2)
+console.log(ans);
+console.log(mark);
+
+const war="hello virendra";
+console.log(war.split(""));
+console.log(war.split(" "));
+console.log(war.split())
+
+
+
+
+
+
+
+
