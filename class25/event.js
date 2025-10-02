1d86ec88708e21a02cf7c1e9ca90f3f269db8599
Virendra Kumar Dwivedi <dwivedivirendrakumar23@gmail.com>
2025-10-02 19:31:50 +0530

    javascript

---
+++ class25/event.js
@@ -0,0 +1,24 @@
+// console.log("Start");
+
+// setTimeout(() => {
+//   console.log("virat kohli");
+// }, 4000);
+
+// console.log("End");
+
+
+// console.log("virendra");
+
+// setTimeout(()=>{
+//   console.log("good morning virat sir")
+// }, 4000);
+
+// console.log("hi virendra Kumar dwivedi")
+
+
+function changeText(){
+let fpara=document.getElementById('fpara');
+fpara.textContent="hello veer"
+}
+let fpara=document.getElementById('fpara');
+fpara.addEventListener('click',changeText());
