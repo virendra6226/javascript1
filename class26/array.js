73d2481ef696550527f6e0f67049c5441d48f67e
Virendra Kumar Dwivedi <dwivedivirendrakumar23@gmail.com>
2025-10-03 22:19:04 +0530

    javascript class26

---
+++ class26/array.js
@@ -0,0 +1,47 @@
+
+// remove the duplicate elements in an array
+
+// const numbers=[1,3,4,5,5,6];
+// const double=[...new Set(numbers)];
+// console.log(double);
+
+
+// // remove the duplicate element in an array
+
+// const num=[2,3,4,5,2,34,56,3,4];
+// const fast= num.filter((n,i)=>{
+//     return num.indexOf(n)===i;
+// })
+// console.log(fast);
+
+//  find the maximum and minimum an array elements
+
+// let  arr=[23,12,13,4,5];
+// let smallest=arr[0];
+// let largest=arr[0];
+// for(let i=1;i<arr.length;i++)
+// {
+//     if(arr[i]<smallest){
+//         smallest=arr[i];
+//     }
+//     if(arr>largest){
+//         largest=arr[i]
+//     }
+// }
+
+// console.log("print the smallest numbers", smallest);
+// console.log("print the largest numbers", largest);
+
+// second type find the max and min array
+
+// reverse an array
+
+// let arr=[23,45,65,76];
+// let arr1=(arr.reverse());
+// console.log(arr1);
+
+//find the second largest  numbers in an array
+
+let quick = [1, 2, 3, 4, 4, 5];
+console.log(quick.indexOf[2])
+
