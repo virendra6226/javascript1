13777f040f62aaf37b0af32252810761b394431d
Virendra Kumar Dwivedi <dwivedivirendrakumar23@gmail.com>
2025-10-04 22:42:28 +0530

    class27

---
+++ class27/string.js
@@ -0,0 +1,196 @@
+// w a p to find the length of string
+
+const strone = "virendra";
+const result = strone.length;
+console.log(strone);
+console.log("find the length string", result);
+
+// w a p to lowercase string
+
+let resul = "VIRENDRA";
+let run = resul.toLowerCase();
+console.log("I miss  you mummy: ", run);
+
+
+
+// w a p to uppercase string
+
+let quick = "virendra";
+let miss = quick.toUpperCase();
+console.log(" I love my family : ", miss);
+
+
+// w a p to compare two string
+
+var str = "veer"
+var str1 = "veer";
+if (str === str1) {
+    console.log("both string are equal");
+}
+else {
+    console.log("both string are not equal");
+}
+
+// w a p to concatenate two string
+
+const brand = "jitendra";
+const tobrand = "udit ";
+const finalbrand = brand + " " + tobrand;
+console.log("string are concatenation", finalbrand);
+
+// w a p to check if a string is empty
+
+const number = "";
+if (number.length === 0) {
+    console.log("string are empty: ")
+}
+else {
+    console.log("string are not empty");
+}
+
+// reverse a string
+
+let num = "23 ,34 ,54";
+let bad = (num.split(" ").reverse().join());
+console.log(bad);
+
+
+// find the length of the longest word in a string
+
+let str= "radhe radhe javascript";
+let words= str.split(" ");
+let maxlength=0;
+for(let word of words){
+    if(word.length>maxlength)
+    {
+        maxlength=word.length;
+    }
+}
+console.log(maxlength);
+
+let min="virendra kumar dwivedi shrikrishna";
+let mins=min.split(" ");
+let num=0;
+for(let number of mins){
+    if(number.length>num)
+    {
+        num=number.length;
+    }
+}
+console.log(num);
+
+
+let brands="har har mahadev";
+let mata=brands.split(" ");
+let jay=0;
+for(let n of mata)
+{
+    if(n.length>jay)
+    {
+        jay=n.length;
+    }
+}
+console.log(jay);
+
+let p="jai jai jai bajrangbali";
+let q=p.split(" ");
+let r=0;
+for(let s of q)
+{
+    if(s.length>r)
+    {
+        r=s.length;
+    }
+}
+console.log(r);
+
+
+// remove the whitespace in string
+
+let rem="  hello virendra  ";
+let final=rem.trim();
+console.log(final);
+
+// replace occurence of a substring within a string
+
+
+// datatypes 
+
+// numbers
+let age=34;
+
+// string 
+let message="jay veeru";
+
+//boolean
+
+let istrue=true;
+
+//undefine
+
+let m;
+console.log(m);
+
+
+// null
+
+let y=null;
+console.log(y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // variables coding with best example
+
+//  a=34;
+//  b=23;
+// console.log(a+b,"addition of two numbers");
+
+//  n=12;
+//  m=23;
+// console.log("Subtraction two numbers are in variables", m-n);
+
+// // multiply 
+//  varaible=43;
+//  variable = 21;
+// console.log("multiply of two numbers", varaible*variable);
+
+
+// fullName="virendra";
+// age=25;
+
+// console.log(fullName, age,"print the all item");
+
